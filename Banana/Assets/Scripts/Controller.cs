13f062f9e8f9e9fd9e4f2215c533d36f5086f1e9
sorami-susumu <sorami.susumu@gmail.com>
2020-09-16 22:11:24 +0900

    part1 simple rainbow road

---
+++ Banana/Assets/Scripts/Controller.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical = Input.GetAxis("Vertical");
+
+        if (horizontal == 0 && vertical == 0) {
+
+        } else {
+            transform.position = new Vector3(
+                transform.position.x + horizontal / 100,
+                transform.position.y,
+                transform.position.z + vertical / 100
+            );
+        }
+    }
+}
